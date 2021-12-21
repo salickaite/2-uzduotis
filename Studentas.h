8ed18f59be37925e5d4667350bb12cfc9e0342a3
salickaite <90153125+salickaite@users.noreply.github.com>
2021-12-21 14:10:49 +0200

    Atnaujinta versija pt2

--- Studentas.h
+++ Studentas.h
@@ -10,39 +10,41 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-class Studentas {
+class Studentas 
+{
 private:
 	string vardas_;
 	string pavarde_;
-	int egzaminas_;
-	float galutinisVid_;
+	int egz_;
+	float gal_vid_;
 	vector<float> nd_;
 
 public:
-	Studentas() {
-		egzaminas_ = 0;
-		galutinisVid_ = 0;
+	Studentas() 
+	{
+		egz_ = 0;
+		gal_vid_ = 0;
 	}
 	Studentas(string, string, int, float);
 
 
 	inline string vardas() const { return vardas_; }
 	inline string pavarde() const { return pavarde_; }
-	inline int egzaminas() const { return egzaminas_; }
+	inline int egzaminas() const { return egz_; }
 	inline vector<float> nd() const { return nd_; }
-	inline float galutinisVid() const { return galutinisVid_; }
+	inline float gal_vid() const { return gal_vid_; }
 
 	void setVardas(string);
 	void setPavarde(string);
 	void setEgzaminas(int);
 	void setND(vector<float>);
-	void setGalutinisVid(float);
+	void setGal_vid(float);
 
 
 	void pild();
 	float vidurkis();
 	float galBalas(const char);
-	static void nuskaitymas_vec_(string read);
+	static void nuskaitymas_v_(string read);
 	static void padalijimas3_(vector<Studentas>& vec);
 	static bool pred_(const Studentas& st);
 
